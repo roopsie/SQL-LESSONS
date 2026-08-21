405048d417a51f7935bf59cf233a319e5b4f657b
roopsie <roopkirandhillon1@gmail.com>
2026-08-21 13:29:13 -0400

    Added a few new files

---
+++ queries/basics.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE students(
+--  id INTEGER PRIMARY KEY,
+--  name TEXT NOT NULL,
+-- age INTEGER,   -marks INTEGER
+--);
+INSERT INTO students VALUES
+(1, 'Alice', 20, 85),
+(2, 'Bob', 22, 90),
+(3, 'Charlie', 19, 78),
+(4, 'David', 21, 92),
+(5, 'Eva', 20, 88);
+
+SELECT * FROM students;
